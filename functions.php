6d1547930aaaf07a06e1c25789bfa041f3005b83
“anne5t85” <anne5t85@stud.kea.dk>
2022-05-19 18:37:19 +0200

    ny

--- functions.php
+++ functions.php
@@ -18,6 +18,7 @@ if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
     function chld_thm_cfg_parent_css() {
         wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array(  ) );
     }
+
 endif;
 add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
 
