a331cf6be60d4673483bd7ce1641d7c1a3dbce0b
“anne5t85” <anne5t85@stud.kea.dk>
2022-05-24 22:00:50 +0200

    kodet side

--- page-husets-events.php
+++ page-husets-events.php
@@ -1,55 +1,424 @@
 
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-    <!-- Bootstrap CSS -->
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU"
-      crossorigin="anonymous"
-    />
-    <link rel="stylesheet" href="style.css" />
-    <title>husets events</title>
-    <!-- Primary Meta Tags -->
-  </head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="http://asoep.dk/kea/post/wp-content/themes/Divi-child/style.css"/></head>
   <body>
   <?php get_header(); ?>
-    <main class="container-fluid">
-
-      <section id="splahsbillede" class="row">
+<main class="container-fluid pl-0 pr-0 " style="background-color:#F7F5EA;">
+<section id="splahsbillede" class="row">
         <div class="slideshow-container">
           <div class="mySlides fade">
             <img
-              src="image/aften.jpg"
+              src="http://asoep.dk/kea/post/wp-content/themes/Divi-child/imgs/event_to_lille.jpg"
               class="img-fluid"
-              alt="aften"
+              alt="forsidebillede"
             />
           </div>
 
-</section>
-    </main>
-  
-    <!-- Optional JavaScript; choose one of the two! -->
+          <div class="mySlides fade">
+            <img
+              src="http://asoep.dk/kea/post/wp-content/themes/Divi-child/imgs/event_syv_lille.jpg"
+              class="img-fluid"
+              alt="forsidebillede"
+            />
+          </div>
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ"
-      crossorigin="anonymous"
-    ></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.8.0/gsap.min.js"></script>
+          <div class="mySlides fade">
+            <img
+              src="http://asoep.dk/kea/post/wp-content/themes/Divi-child/imgs/event_fem_lille.jpg"
+              class="img-fluid"
+              alt="forsidebillede"
+            />
+          </div>
+          <div class="mySlides fade">
+            <img
+              src="http://asoep.dk/kea/post/wp-content/themes/Divi-child/imgs/event_tre_lille.jpg"
+              class="img-fluid"
+              alt="forsidebillede"
+            />
+          </div>
+          <div class="mySlides fade">
+            <img
+              src="http://asoep.dk/kea/post/wp-content/themes/Divi-child/imgs/event_seks_lille.jpg"
+              class="img-fluid"
+              alt="forsidebillede"
+            />
+          </div>
+          <div class="mySlides fade">
+            <img
+              src="http://asoep.dk/kea/post/wp-content/themes/Divi-child/imgs/event_et_lille.jpg"
+              class="img-fluid"
+              alt="forsidebillede"
+            />
+          </div>
+        </div>
+
+        <div style="text-align: center">
+          <span class="dot"></span>
+          <span class="dot"></span>
+          <span class="dot"></span>
+          <span class="dot"></span>
+          <span class="dot"></span>
+          <span class="dot"></span>
+        </div>
+      </section>
+      <section class="overskrift pt-4 m-4 lg-m-0">
+      <div class="top col-md-6">
+      <h1 class="overskrift pb-4 pt-4 mt-2"> Husets events</h1> 
+      <h3 class="underoverskrift pb-4 pt-2" style="color:#767458";>
+      Hos POST holder vi løbende forskellige events. 
+Tjek siden her og hold dig opdateret på hvad 
+der sker, i POST huset.</h3>
+<hr class="streg pb-5">
+</div>
+</section>
+<section class="page-section bg-light mt-3" id="portfolio">
+        <div class="container_to" style="margin-left: 6rem;margin-right: 5rem;">
+          <div class="text-center"></div>
+          <div class="row">
+            <div class="col-lg-6 mb-4">
+              <!-- Portfolio item 1-->
+              <div class="portfolio-item">
+                <a
+                  class="portfolio-link"
+                  data-bs-toggle="modal"
+                  href="#portfolioModal1"
+                >
+                  <div class="portfolio-hover">
+                  </div>
+                  <img
+                    class="img-fluid"
+                    src="http://asoep.dk/kea/post/wp-content/themes/Divi-child/imgs/fest.jpg"
+                    alt="..."
+                  />
+                </a>
+                <div class="portfolio-caption">
+                  <div class="portfolio-caption-heading text-uppercase">
+                    Åbningsfest på post
+                  </div>
+                  <a
+                  class="portfolio-link"
+                  data-bs-toggle="modal"
+                  href="#portfolioModal1"> læs mere </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6 mb-4">
+              <!-- Portfolio item 2-->
+              <div class="portfolio-item">
+                <a
+                  class="portfolio-link"
+                  data-bs-toggle="modal"
+                  href="#portfolioModal2"
+                >
+                  <div class="portfolio-hover">
+                
+                  </div>
+                  <img class="img-fluid" src="http://asoep.dk/kea/post/wp-content/themes/Divi-child/imgs/banko.jpg" alt="..." />
+                </a>
+                <div class="portfolio-caption">
+                  <div class="portfolio-caption-heading text-uppercase">
+                    Pizza banko
+                  </div>
+                  <a
+                  class="portfolio-link"
+                  data-bs-toggle="modal"
+                  href="#portfolioModal2"> læs mere </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6 mb-4">
+              <!-- Portfolio item 3-->
+              <div class="portfolio-item">
+                <a
+                  class="portfolio-link"
+                  data-bs-toggle="modal"
+                  href="#portfolioModal3"
+                >
+              
+                  <div class="portfolio-hover">
+                    
+                  </div>
+                  <img
+                    class="img-fluid"
+                    src="http://asoep.dk/kea/post/wp-content/themes/Divi-child/imgs/breve.jpg"
+                    alt="..."
+                  />
+                </a>
+                <div class="portfolio-caption">
+                  <div class="portfolio-caption-heading text-uppercase">
+                  folkekøkken og breve
+                  </div>
+                
+                <a
+                  class="portfolio-link"
+                  data-bs-toggle="modal"
+                  href="#portfolioModal3"> læs mere </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6 mb-5">
+              <!-- Portfolio item 4-->
+              <div class="portfolio-item">
+                <a
+                  class="portfolio-link"
+                  data-bs-toggle="modal"
+                  href="#portfolioModal4"
+                >
+                  <div class="portfolio-hover">
+                   
+                  </div>
+                  <img
+                    class="img-fluid"
+                    src="http://asoep.dk/kea/post/wp-content/themes/Divi-child/imgs/talk.jpg"
+                    alt="..."
+                  />
+                </a>
+                <div class="portfolio-caption">
+                  <div class="portfolio-caption-heading text-uppercase">
+                    Samtalekøkken og god vin 
+                  </div>
+                  <a
+                  class="portfolio-link"
+                  data-bs-toggle="modal"
+                  href="#portfolioModal4"> læs mere </a>
+                </div>
+              </div>
+            </div>
+           
+      </section>
+      <!-- Portfolio item 1 modal popup-->
+      <div
+        class="portfolio-modal modal fade"
+        id="portfolioModal1"
+        tabindex="-1"
+        role="dialog"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="close-modal" data-bs-dismiss="modal">
+              <img src="http://asoep.dk/kea/post/wp-content/themes/Divi-child/imgs/kryds.svg" alt="Close modal" />
+            </div>
+            <div class="container">
+              <div class="row justify-content-center">
+                <div class="col-lg-8">
+                  <div class="modal-body">
+                    <!-- Project details-->
+                    <h2 class="text-uppercase">Åbningsfest på POST</h2>
+                    <p class="item-intro text-muted text-left">
+                      Journalistic methods and journalistic content production
+                    </p>
+                    <!-- <img
+                      class="img-fluid d-block mx-auto"
+                      src="image_web/web.gif"
+                      alt="..."
+                    /> -->
+                    <p>
+                      Formålet med denne opgave var at få erfaring med
+                      journalistiske metoder og journalistisk
+                      indholdsproduktion. Hovedfokuset var at skabe værdi gennem
+                      design og finde koblingen mellem design, kommunikation og
+                      indholdsproduktion.
+                    </p>
+                    <p>
+                      Min opgave var at udarbejde indhold, design og skabe en
+                      Webfeature som dokumenterede en livssituation. Min
+                      Webfeature er produceret i WordPress og Divi. Videoer og
+                      billeder er taget med en osmo pocket og de grafiske
+                      elementer er produceret i adobe illustrator. Målet var at
+                      markedsføre websitet med engagerende indhold til SoMe, som
+                      skulle lægge op til debat om selve webfeaturens emne.
+                    </p>
+
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Portfolio item 2 modal popup-->
+      <div
+        class="portfolio-modal modal fade"
+        id="portfolioModal2"
+        tabindex="-1"
+        role="dialog"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="close-modal" data-bs-dismiss="modal">
+              <img src="http://asoep.dk/kea/post/wp-content/themes/Divi-child/imgs/kryds.svg" alt="Close modal" />
+            </div>
+            <div class="container">
+              <div class="row justify-content-center">
+                <div class="col-lg-8">
+                  <div class="modal-body">
+                    <!-- Project details-->
+                    <h2 class="text-uppercase">Pizza banko</h2>
+                    <p class="item-intro text-muted text-left">
+                      Content Management System and dynamic web solutions
+                    </p>
+                    <!-- <img
+                      class="img-fluid d-block mx-auto"
+                      src="image_web/radio.png"
+                      alt="..."
+                    /> -->
+                    <p>
+                      Formålet med denne opgave var at udarbejde en webløsning,
+                      der formidler Radio LOUD på en nem og overskuelig måde,
+                      der samtidig er indbydende og udstråler troværdighed og
+                      professionalisme. Løsningen skulle fremme lyttertal og
+                      engagement hos målgruppen og samtidig være en løsning på
+                      tværs af div. devices, der gjorde det lettere at navigere
+                      i kanalens indhold.
+                    </p>
+                    <p>
+                      Opgaven blev løst ved at skabe et website, hvor
+                      program-indholdet blev hentet dynamisk fra WordPress med
+                      WP rest API. Analyse af nuværende website samt andre
+                      analyser, metoder og indsigter, der ledte frem til den
+                      endelig løsnings IA struktur og design. Udarbejde en
+                      målgruppeanalyse samt indsigter med henblik på
+                      personabeskrivelser, der repræsenterede Radio LOUDs brede
+                      målgruppe.
+                    </p>
+
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Portfolio item 3 modal popup-->
+      <div
+        class="portfolio-modal modal fade"
+        id="portfolioModal3"
+        tabindex="-1"
+        role="dialog"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="close-modal" data-bs-dismiss="modal">
+              <img src="http://asoep.dk/kea/post/wp-content/themes/Divi-child/imgs/kryds.svg" alt="Close modal" />
+            </div>
+            <div class="container">
+              <div class="row justify-content-center">
+                <div class="col-lg-8">
+                  <div class="modal-body">
+                    <!-- Project details-->
+                    <h2 class="text-uppercase">folkekøkken og breve</h2>
+                    <p class="item-intro text-muted text-left">
+                      Multimedia solution for a danish entrepreneur
+                    </p>
+                    <!-- <img
+                      class="img-fluid d-block mx-auto"
+                      src="image_web/sugar1.jpg"
+                      alt="..."
+                    /> -->
+                    <p>
+                      Formålet med denne opgave var at give en dansk iværksætter
+                      en hjælpende hånd med nye innovative løsninger i det
+                      digitale univers. Målet var at udarbejde et website, der
+                      skulle promovere den valgte klients produkter / ydelser,
+                      idéer og værdier. Sitet skulle illustrere stil, sprogbrug,
+                      navigation, funktionalitet samt brugervenlighed. Der
+                      skulle skabes en visuel ID præsenteret gennem en
+                      designguide, som afspejlede klientens værdier.
+                    </p>
+                    <p>
+                      Til løsningen af opgaven blev der indgået et samarbejde
+                      med virksomheden Sugar Pilots, som udvikler søde sager,
+                      der overrasker og inspirerer. Sugar Pilots signaturprodukt
+                      er slikkepinde. De kan personliggøres og skræddersyes, så
+                      de matcher kundens ønsker både til private kunder og
+                      virksomheder. Formålet var her at forbedre Sugar Pilots
+                      digitale platforme og udvikle en stærkere visuel identitet
+                      gennem indsigter fra research, analyser og test.
+                    </p>
+
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Portfolio item 4 modal popup-->
+      <div
+        class="portfolio-modal modal fade"
+        id="portfolioModal4"
+        tabindex="-1"
+        role="dialog"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="close-modal" data-bs-dismiss="modal">
+              <img src="http://asoep.dk/kea/post/wp-content/themes/Divi-child/imgs/kryds.svg" alt="Close modal" />
+            </div>
+            <div class="container">
+              <div class="row justify-content-center">
+                <div class="col-lg-8">
+                  <div class="modal-body">
+                    <!-- Project details-->
+                    <h2 class="text-uppercase">samtalekøkken og god vin</h2>
+                    <p class="item-intro text-muted text-left">
+                      Content marketing
+                    </p>
+                    <!-- <img
+                      class="img-fluid d-block mx-auto"
+                      src="image_web/tundase.png"
+                      alt="..."
+                    /> -->
+                    <p>
+                      Formålet med denne opgave var at få erfaring med
+                      indholdsmarkedsføring, kreativ indholdsproduktion samt
+                      design og udvikling af et mikrosite.
+                    </p>
+                    <p>
+                      Min opgave var at redesigne og rebrand en fiktiv
+                      dagligvare. Skabe opmærksomhed via SEO og sprede indhold,
+                      der skulle skabe personlige relationer mellem produkt og
+                      målgruppe via SoMe. Til at løse opgaven havde jeg fokus på
+                      brandpersonligheder til at koble design, marketing og
+                      indholdsproduktion, for at kunne sikre indholdet emotionel
+                      værdi for målgruppen. Mikrositet er kodet med Boostrap og
+                      design indholdet er produceret med adobe photoshop og
+                      adobe illustrator.
+                    </p>
+
+                  
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+     
+</section>
+</main>
+
+
+<?php get_footer(); ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.8.0/gsap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.8.0/ScrollTrigger.min.js"></script>
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
-    -->
-    <script src="script.js"></script>
-	<?php get_footer(); ?>
+<script src="http://asoep.dk/kea/post/wp-content/themes/Divi-child/js/script.js"></script>
   </body>
+   
 </html>
+     
